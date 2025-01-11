b7cab088bfd8c584e205218f07b126d2bfa16ee0
Joshua Kakinuki <josh.kakinuki@students.dominican.edu>
2025-01-10 17:27:58 -0800

    Updating projucer app

--- AudioRouterApp/Source/ZeroMQServer.cpp
+++ AudioRouterApp/Source/ZeroMQServer.cpp
@@ -15,3 +15,40 @@ ZeroMQServer::ZeroMQServer(AudioEngine& engine) : audioEngine(engine)
     }
 }
 
+void ZeroMQServer::listen()
+{
+    zmq::pollitem_t items[] = { { static_cast<void*>(socket), 0, ZMQ_POLLIN, 0 } };
+
+    while (true)
+    {
+        try
+        {
+            zmq::poll(&items[0], 1, std::chrono::milliseconds(10)); // Poll every 10 ms
+
+            if (items[0].revents & ZMQ_POLLIN)
+            {
+                zmq::message_t request;
+                auto recvResult = socket.recv(request, zmq::recv_flags::none);
+                if (!recvResult)
+                {
+                    std::cerr << "ZeroMQ receive error!" << std::endl;
+                    continue; // Retry instead of exiting
+                }
+
+                // Handle received message
+                std::string msg(static_cast<char*>(request.data()), request.size());
+                std::cerr << "Received command: " << msg << std::endl; // Debugging log
+                audioEngine.handleCommand(msg);
+
+                // Send reply
+                zmq::message_t reply(5);
+                memcpy(reply.data(), "Done", 5);
+                socket.send(reply, zmq::send_flags::none);
+            }
+        }
+        catch (const zmq::error_t& e)
+        {
+            std::cerr << "ZeroMQ server error: " << e.what() << std::endl;
+        }
+    }
+}
