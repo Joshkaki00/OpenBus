a880106341e80d265182f6149e13dae24c893814
Joshua Kakinuki <josh.kakinuki@students.dominican.edu>
2025-01-10 17:30:16 -0800

    Updating projucer app

--- AudioRouterApp/Source/AudioEngine.cpp
+++ AudioRouterApp/Source/AudioEngine.cpp
@@ -8,3 +8,20 @@ AudioEngine::AudioEngine()
     setupGraph();
 }
 
+// Setup Audio Graph
+void AudioEngine::setupGraph()
+{
+    try
+    {
+        for (int i = 0; i < maxInputs; ++i)
+        {
+            inputNode[i] = addNode(createIOProcessor(true)).get();
+            outputNode[i] = addNode(createIOProcessor(false)).get();
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Graph setup failed: " << e.what() << std::endl;
+    }
+}
+
