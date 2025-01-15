4e87d2beb807370a249a4f46c5e30f03376bd0b5
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-01-14 19:13:05 -0800

    fixing bugs

--- AudioRouterApp/Source/MainComponent.h
+++ AudioRouterApp/Source/MainComponent.h
@@ -6,19 +6,24 @@
 class MainComponent : public juce::Component
 {
 public:
+    // Constructor and Destructor
     MainComponent();
     ~MainComponent() override = default;
 
+    // Component Overrides
     void resized() override;
 
 private:
+    // Button click handlers
     void onLoadPlugin();
     void onSavePreset();
     void onLoadPreset();
 
+    // UI Components
     juce::TextButton loadPluginButton{"Load Plugin"};
     juce::TextButton savePresetButton{"Save Preset"};
     juce::TextButton loadPresetButton{"Load Preset"};
 
+    // Prevent copy and move
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
