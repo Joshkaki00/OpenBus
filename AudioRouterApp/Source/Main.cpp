2dd0844c3571946f82e734b5349104ff29314039
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-01-14 19:27:33 -0800

    fixing bugs

--- AudioRouterApp/Source/Main.cpp
+++ AudioRouterApp/Source/Main.cpp
@@ -4,33 +4,12 @@
 class AudioRouterApp : public juce::JUCEApplication
 {
 public:
-    class MainWindow : public juce::DocumentWindow
-    {
-    public:
-        MainWindow(const juce::String& name, juce::Component* component)
-            : juce::DocumentWindow(name, juce::Colours::lightgrey, juce::DocumentWindow::allButtons)
-        {
-            setUsingNativeTitleBar(true);
-            setContentOwned(component, true);
-            setResizable(true, true);
-            centreWithSize(getWidth(), getHeight());
-            setVisible(true);
-        }
-
-        void closeButtonPressed() override
-        {
-            juce::JUCEApplication::getInstance()->systemRequestedQuit();
-        }
-    };
-
-    AudioRouterApp() = default;
-
     const juce::String getApplicationName() override { return "AudioRouterApp"; }
     const juce::String getApplicationVersion() override { return "1.0.0"; }
 
     void initialise(const juce::String&) override
     {
-        mainWindow.reset(new MainWindow("AudioRouterApp", new MainComponent()));
+        mainWindow.reset(new MainWindow("AudioRouterApp", new MainComponent(), *this));
     }
 
     void shutdown() override
@@ -39,91 +18,30 @@ public:
     }
 
 private:
+    class MainWindow : public juce::DocumentWindow
+    {
+    public:
+        MainWindow(const juce::String& name, juce::Component* c, juce::JUCEApplication& app)
+            : juce::DocumentWindow(name, juce::Colours::lightgrey, juce::DocumentWindow::allButtons),
+              application(app)
+        {
+            setUsingNativeTitleBar(true);
+            setContentOwned(c, true);
+            setResizable(true, true);
+            centreWithSize(getWidth(), getHeight());
+            setVisible(true);
+        }
+
+        void closeButtonPressed() override
+        {
+            application.systemRequestedQuit();
+        }
+
+    private:
+        juce::JUCEApplication& application;
+    };
+
     std::unique_ptr<MainWindow> mainWindow;
 };
 
-MainComponent::MainComponent()
-{
-    loadPluginButton.onClick = [this]() { onLoadPlugin(); };
-    savePresetButton.onClick = [this]() { onSavePreset(); };
-    loadPresetButton.onClick = [this]() { onLoadPreset(); };
-
-    addAndMakeVisible(loadPluginButton);
-    addAndMakeVisible(savePresetButton);
-    addAndMakeVisible(loadPresetButton);
-
-    setSize(600, 400);
-}
-
-void MainComponent::resized()
-{
-    auto area = getLocalBounds().reduced(10);
-    auto buttonHeight = 40;
-
-    loadPluginButton.setBounds(area.removeFromTop(buttonHeight).reduced(5));
-    savePresetButton.setBounds(area.removeFromTop(buttonHeight).reduced(5));
-    loadPresetButton.setBounds(area.removeFromTop(buttonHeight).reduced(5));
-}
-
-void MainComponent::onLoadPlugin()
-{
-    juce::FileChooser chooser("Select a plugin to load...", {}, "*.vst3;*.vst;*.component");
-    chooser.launchAsync(juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectFiles,
-        [this](const juce::FileChooser& fc)
-        {
-            auto file = fc.getResult();
-            if (file.existsAsFile())
-            {
-                if (!AudioEngine::getInstance().loadPlugin(file))
-                {
-                    juce::AlertWindow::showMessageBoxAsync(
-                        juce::AlertWindow::WarningIcon,
-                        "Error",
-                        "Failed to load plugin."
-                    );
-                }
-            }
-        });
-}
-
-void MainComponent::onSavePreset()
-{
-    juce::FileChooser chooser("Save Preset...", {}, "*.preset");
-    chooser.launchAsync(juce::FileBrowserComponent::saveMode | juce::FileBrowserComponent::canSelectFiles,
-        [this](const juce::FileChooser& fc)
-        {
-            auto file = fc.getResult();
-            if (file.hasWriteAccess())
-            {
-                if (!AudioEngine::getInstance().savePreset(file))
-                {
-                    juce::AlertWindow::showMessageBoxAsync(
-                        juce::AlertWindow::WarningIcon,
-                        "Error",
-                        "Failed to save preset."
-                    );
-                }
-            }
-        });
-}
-
-void MainComponent::onLoadPreset()
-{
-    juce::FileChooser chooser("Load Preset...", {}, "*.preset");
-    chooser.launchAsync(juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectFiles,
-        [this](const juce::FileChooser& fc)
-        {
-            auto file = fc.getResult();
-            if (file.existsAsFile())
-            {
-                if (!AudioEngine::getInstance().loadPreset(file))
-                {
-                    juce::AlertWindow::showMessageBoxAsync(
-                        juce::AlertWindow::WarningIcon,
-                        "Error",
-                        "Failed to load preset."
-                    );
-                }
-            }
-        });
-}
+START_JUCE_APPLICATION(AudioRouterApp)
