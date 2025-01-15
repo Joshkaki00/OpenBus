ed4698af44a777ebf1c42d89b0415caad506fce6
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-01-14 19:21:30 -0800

    fixing bugs

--- AudioRouterApp/Source/ZeroMQServer.cpp
+++ AudioRouterApp/Source/ZeroMQServer.cpp
@@ -16,7 +16,12 @@ void ZeroMQServer::listen()
     while (true)
     {
         zmq::message_t request;
-        socket.recv(request, zmq::recv_flags::none);
+        auto result = socket.recv(request, zmq::recv_flags::none);
+        if (!result)
+        {
+            std::cerr << "Failed to receive message from socket." << std::endl;
+            return; // Or handle the error appropriately
+        }
 
         try
         {
