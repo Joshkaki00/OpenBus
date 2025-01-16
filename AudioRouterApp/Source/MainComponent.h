b0714c0d556e6c7454df4daa1109eee6489b8268
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-01-16 14:54:45 -0800

    fixing bugs

--- AudioRouterApp/Source/MainComponent.h
+++ AudioRouterApp/Source/MainComponent.h
@@ -3,6 +3,26 @@
 #include <JuceHeader.h>
 #include "AudioEngine.h"
 
+// Custom LookAndFeel class to customize fonts
+class CustomLookAndFeel : public juce::LookAndFeel_V4
+{
+public:
+    CustomLookAndFeel()
+    {
+        setDefaultSansSerifTypefaceName("Arial"); // Optional: Set default font family
+    }
+
+    juce::Font getComboBoxFont(juce::ComboBox&) override
+    {
+        return juce::Font(18.0f); // Set font size for ComboBox
+    }
+
+    juce::Font getLabelFont(juce::Label&) override
+    {
+        return juce::Font(18.0f); // Set font size for Label
+    }
+};
+
 class MainComponent : public juce::Component
 {
 public:
@@ -26,6 +46,9 @@ private:
     juce::ComboBox hardwareInputsMenu, virtualInputsMenu, hardwareOutMenu;
     juce::Label hardwareInputsLabel{"Hardware Inputs"}, virtualInputsLabel{"Virtual Inputs"}, hardwareOutLabel{"Hardware Outputs"};
     juce::AudioDeviceManager audioDeviceManager;
+
+    // Custom LookAndFeel instance
+    CustomLookAndFeel customLookAndFeel;
 
     // Prevent copy and move
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
