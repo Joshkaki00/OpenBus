bb7179d28c80b1f1eb8a1f393304119c219efacb
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-01-16 16:00:49 -0800

    decreased dropdown size

--- AudioRouterApp/Source/MainComponent.cpp
+++ AudioRouterApp/Source/MainComponent.cpp
@@ -70,17 +70,17 @@ void MainComponent::resized()
 
     // Layout hardware inputs
     hardwareInputsLabel.setBounds(area.removeFromTop(labelHeight));
-    hardwareInputsMenu.setBounds(area.removeFromTop(dropdownHeight).reduced(0, 5));
+    hardwareInputsMenu.setBounds(area.removeFromTop(dropdownHeight).reduced(0, 1));
     area.removeFromTop(verticalSpacing); // Add spacing
 
     // Layout virtual inputs
     virtualInputsLabel.setBounds(area.removeFromTop(labelHeight));
-    virtualInputsMenu.setBounds(area.removeFromTop(dropdownHeight).reduced(0, 5));
+    virtualInputsMenu.setBounds(area.removeFromTop(dropdownHeight).reduced(0, 1));
     area.removeFromTop(verticalSpacing); // Add spacing
 
     // Layout hardware outputs
     hardwareOutLabel.setBounds(area.removeFromTop(labelHeight));
-    hardwareOutMenu.setBounds(area.removeFromTop(dropdownHeight).reduced(0, 5));
+    hardwareOutMenu.setBounds(area.removeFromTop(dropdownHeight).reduced(0, 1));
 }
 
 void MainComponent::setupDropdown(juce::ComboBox& dropdown, const juce::String& labelText, juce::Label& label)
